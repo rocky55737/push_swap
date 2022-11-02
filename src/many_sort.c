ac0e6f9355972c995d62985e434dd6b26038a1ff
RHONG <rocky55737@gmail.com>
2022-11-02 17:44:38 +0900

    three sort not linked by sa

--- src/many_sort.c
+++ src/many_sort.c
@@ -6,7 +6,7 @@
 /*   By: rhong <rhong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 16:28:28 by rhong             #+#    #+#             */
-/*   Updated: 2022/11/02 16:44:59 by rhong            ###   ########.fr       */
+/*   Updated: 2022/11/02 17:42:05 by rhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,32 @@ void	print_deq(t_deqs *deqs)
 		tmp = tmp->back;
 	}
 	ft_printf("\n");
+	ft_printf("deq_a: ");
+	tmp = deqs->deq_a;
+	while (tmp->back)
+		tmp = tmp->back;
+	while (tmp)
+	{
+		ft_printf("%d ", tmp->data);
+		tmp = tmp->front;
+	}
+	ft_printf("\n");
 	ft_printf("deq_b: ");
 	tmp = deqs->deq_b;
 	while (tmp)
 	{
 		ft_printf("%d ", tmp->data);
 		tmp = tmp->back;
+	}
+	ft_printf("\n");
+	ft_printf("deq_b: ");
+	tmp = deqs->deq_b;
+	while (tmp->back)
+		tmp = tmp->back;
+	while (tmp)
+	{
+		ft_printf("%d ", tmp->data);
+		tmp = tmp->front;
 	}
 	ft_printf("\n");
 }
@@ -58,8 +78,10 @@ t_deqs	*many_sort(t_deqs *deqs)
 	
 	while (deq_len(deqs->deq_a) != 3)
 		deqs = pb(deqs);
+	print_deq(deqs);
 
 	deqs = sort_three(deqs);
+	print_deq(deqs);
 
 	while (deq_len(deqs->deq_b) != 0)
 	{
@@ -69,12 +91,10 @@ t_deqs	*many_sort(t_deqs *deqs)
 			{
 				if (deq_len(deqs->deq_a) / 2 > deq_get_index_min(deqs->deq_a))
 				{
-					ft_printf("ra\n");
 					deqs = ra(deqs);
 				}
 				else
 				{
-					ft_printf("rra\n");
 					deqs = rra(deqs);
 				}
 			}
@@ -83,24 +103,17 @@ t_deqs	*many_sort(t_deqs *deqs)
 		{
 			while (deq_get_index_big(deqs->deq_a, deqs->deq_b->index) != 0)
 			{
-				print_deq(deqs);
 				if (deq_len(deqs->deq_a) / 2 > deq_get_index_big(deqs->deq_a, deqs->deq_b->index))
 				{
-					ft_printf("ra\n");
 					deqs = ra(deqs);
 				}
 				else
 				{
-					ft_printf("rra\n");
 					deqs = rra(deqs);
-					print_deq(deqs);
 				}
 			}
 		}
-		print_deq(deqs);
-		ft_printf("pa\n");
 		deqs = pa(deqs);
-		print_deq(deqs);
 	}
 
 	while (deqs->deq_a->index != 0)
@@ -114,5 +127,6 @@ t_deqs	*many_sort(t_deqs *deqs)
 			deqs = ra(deqs);
 		}
 	}
+	print_deq(deqs);
 	return (deqs);
 }
