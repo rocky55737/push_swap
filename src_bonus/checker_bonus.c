bb858fe5f68714705f1960cef57dcaf100109897
록기 <rocky55737@gmail.com>
2022-11-05 18:47:04 +0900

    sort_bonus

--- src_bonus/checker_bonus.c
+++ src_bonus/checker_bonus.c
@@ -1,1 +1,16 @@
 #include "push_swap_bonus.h"
+
+void	checker(int ac, char **av)
+{
+	t_deqs	*deqs;
+
+	if (input_err(ac, av))
+		exit(1);
+	deqs = (t_deqs *)malloc(sizeof(t_deqs));
+	malloc_null_guard(deqs);
+	deqs->deq_a = push_data_to_deq(ac, av);
+	deqs->deq_b = 0;
+	deqs->deq_a = set_index(deqs->deq_a);
+	deqs = sort(deqs);
+	return ;
+}
--- src_bonus/sort_bonus.c
+++ src_bonus/sort_bonus.c
@@ -1,28 +1,46 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rhong <rhong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/21 13:17:24 by rhong             #+#    #+#             */
-/*   Updated: 2022/10/25 08:51:03 by rhong            ###   ########.fr       */
+/*   Created: 2022/10/24 16:22:15 by rhong             #+#    #+#             */
+/*   Updated: 2022/11/04 18:06:18 by rhong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap_bonus.h"
 
-void	push_swap(int ac, char **av)
-{
-	t_deqs	*deqs;
+void	sort(t_deqs *deqs);
+int		is_sorted(t_deqs *deqs, int total_len);
 
-	if (input_err(ac, av))
-		exit(1);
-	deqs = (t_deqs *)malloc(sizeof(t_deqs));
-	malloc_null_guard(deqs);
-	deqs->deq_a = push_data_to_deq(ac, av);
-	deqs->deq_b = 0;
-	deqs->deq_a = set_index(deqs->deq_a);
-	deqs = sort(deqs);
-	exit(0);
+void	sort(t_deqs *deqs)
+{
+	char	*buffer;
+
+	if (is_sorted(deqs, deq_len(deqs->deq_a)))
+	{
+		buffer = 
+		if ()
+	}
+}
+
+int	is_sorted(t_deqs *deqs, int total_len)
+{
+	int		index;
+	t_node	*tmp;
+
+	index = 0;
+	tmp = deqs->deq_a;
+	while (tmp && index == tmp->index)
+	{
+		tmp = tmp->back;
+		index++;
+	}
+	if (total_len != index)
+	{
+		return (0);
+	}
+	return (1);
 }
